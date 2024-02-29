96de23e2b730548fe57c9dc3f967f1e1e0021e52
gapry <gapry@protonmail.com>
2024-02-29 10:47:07 +0800

    WeirdAlgorithm: ACCEPTED

---
+++ 001-WeirdAlgorithm/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+  uint64_t n = 0;
+  std::cin >> n;
+  while (n > 1) {
+    std::cout << n << " ";
+    n = (!(n & 1)) ? n >> 1 : n + (n << 1) + 1;
+  }
+  std::cout << n << "\n";
+  return 0;
+}
