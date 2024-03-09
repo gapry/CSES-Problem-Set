477bebb45ad8cd8cea5fdc3365cbae55495ef142
gapry <gapry@protonmail.com>
2024-03-09 08:31:42 +0800

    VSCodeTemplate: Update

--- VSCodeTemplate/main.cpp
+++ VSCodeTemplate/main.cpp
@@ -1,10 +1,7 @@
+// Task:
+
 #include <iostream>
-#include <string>
 
 int main(void) {
-  std::string msg;
-  while (std::getline(std::cin, msg)) {
-    std::cout << msg << "\n";
-  }
   return 0;
 }
